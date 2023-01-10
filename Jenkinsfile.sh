ba1cc189dcaeb31bce6e5ba437e55fb79915e14e
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-10 13:36:23 +0200

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -26,3 +26,18 @@ pipeline {
     }
   }
 }
+
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  --publish 3000:3000 --publish 5000:5000 \
+  docker:dind \
+  --storage-driver overlay2 
