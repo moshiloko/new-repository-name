57c1cfa035b8f539a3d49e180250ae85853575d7
moshiloko <moshesamimi1999@gmail.com>
2023-01-11 10:15:53 +0200

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,43 +1,5 @@
-pipeline {
-  agent any
-    
-  tools {nodejs "node"}
-    
-  stages {
-        
-    stage('Git') {
-      steps {
-        git 'https://github.com/moshiloko/new-repository-name.git'
-      }
-    }
-     
-    stage('Build') {
-      steps {
-        sh 'npm install'
-         sh 'npm run build'
-      }
-    }  
-    
-            
-    stage('Test') {
-      steps {
-        sh 'node test'
-      }
-    }
-  }
-}
-
-docker run \
-  --name jenkins-docker \
-  --rm \
-  --detach \
-  --privileged \
-  --network jenkins \
-  --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 \
-  --publish 3000:3000 --publish 5000:5000 \
-  docker:dind \
-  --storage-driver overlay2 
+#!/bin/bash
+git pull
+npm install
+npm run build
+serve -n build
