060e6f6dacae1da600d90063d77bb6fcb57988f7
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-17 13:33:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HELLO W
+         HELLO
         </p>
         <a
           className="App-link"
