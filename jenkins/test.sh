b68b71e661d243ad80da5f65a2db31c2fc7320e8
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-17 15:19:23 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 pm2 start test.sh
-sudo env PATH=$PATH:/var/lib/jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/nodejs/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u jenkins --hp /var/lib/jenkins
-pm2 startup
 npm run build
-PORT=3000 forever start -c serve -n build
+serve -n build &
