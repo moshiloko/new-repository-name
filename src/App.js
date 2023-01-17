c3d5c7e433e55876f6d301f7ca3cebc187d64c3d
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-17 16:07:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         moshe app
+         moshe
         </p>
         <a
           className="App-link"
