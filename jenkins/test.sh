9d499e3bbd9ee5b5bf09ee6942d5e9d0dfc57557
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-19 10:04:47 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/bash
 
 lsof -i tcp:3000 
 
-#pm2 start test.sh --watch
-#pm2 save
-#npm start
-#npm run build
-#serve -n build&
+# pm2 start test.sh --watch
+# pm2 save
+# npm start
+# npm run build
+# serve -n build&
