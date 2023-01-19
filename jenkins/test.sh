a4fcf2e22a60f6ecf9a9947b64ff456994bfea4b
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-19 14:16:29 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 #pm2 restart app --watch
 pm2 start 'npm start' --watch
-pm2 save
-pm2 status
 #npm start
 pm2 start 'npm run build' --watch
 pm2 start 'serve -s build' --watch 
+pm2 save
+pm2 status
