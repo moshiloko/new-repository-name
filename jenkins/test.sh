e64cb72f15e2f44520f131365137cc0235cd38ff
Refael Sasson <moshesamimi1999@gmail.com>
2023-01-24 11:22:22 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # pm2 restart app --watch
 # pm2 save
-pm2 start 'serve -s build' --watch
-npm run build
-pm2 restart "serve -s build"
+# pm2 start 'serve -s build' --watch
+# npm run build
+# pm2 restart "serve -s build"
+npm i
