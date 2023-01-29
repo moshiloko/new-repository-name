9c948138ba7418bc881884adeaf81439c12bd040
moshiloko <moshesamimi1999@gmail.com>
2023-01-29 15:44:50 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,9 +1,2 @@
-# !/usr/bin/bash
-#  pm2 restart app --watch
-#  pm2 save
-#  echo "hello world"
 npm run build
-#pm2 serve build 3000 --spa &
 pm2 start serve -- -s build
-#pm2 restart 0 &
-# serve -s build 
