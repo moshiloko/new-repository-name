74649b18f72991e7f39da1086b5aa269d2de7a6a
moshiloko <moshesamimi1999@gmail.com>
2023-01-29 10:49:52 +0200

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,8 +3,8 @@
 # pm2 save
  pm2 start --name "my build" -- serve -s build
 # sudo npm i
-pm2 restart "serve -s build"
+# pm2 restart "serve -s build"
 # echo "hello world"
-# npm run build
-# pm2 restart 0
-# pm2 serve build 3000 --spa
+ npm run build
+pm2 restart 0
+ pm2 serve build 3000 --spa
