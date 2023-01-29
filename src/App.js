91bba1e996d227c8ec646f9ba2db0facb027f006
moshiloko <moshesamimi1999@gmail.com>
2023-01-29 16:27:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hi cosrabakers
+        hi cosrabakers
         </p>
         <a
           className="App-link"
