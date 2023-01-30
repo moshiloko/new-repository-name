0b11d4e6a5c80bd9e048b3df00c964aefc4f6d4d
moshiloko <moshesamimi1999@gmail.com>
2023-01-30 11:08:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hi im working
+        יש לי אדום מתחתיי?
         </p>
         <div style={{width: 50 , height: 50 , backgroundColor:'red'}}/>
         <a
