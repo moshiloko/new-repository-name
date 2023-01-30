e6e5307b06ffa5744b215ba203c7ce8d7f261e7a
moshiloko <moshesamimi1999@gmail.com>
2023-01-30 10:25:15 +0200

    test

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,4 +1,5 @@
-npm run build
+# npm run build
 # pm2 restart serve -- -s build --watch
 # pm2 start serve -- -s build --watch
-serve -s build -l 3000
+# serve -s build -l 3000
+npm start
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hi cosrabakers
+        hi world
         </p>
         <a
           className="App-link"
