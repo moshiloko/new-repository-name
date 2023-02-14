a8a3f9099c4b450964fc2551048829ca037c702c
rafaf077 <111489413+moshiloko@users.noreply.github.com>
2023-02-14 16:36:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          his Is Awesome
+          This Is Fucking FUCK Awesome!! 
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
