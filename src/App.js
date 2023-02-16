be7af8f58640ffbe7aee3329f0427e7b42e2b3cc
rafaf077 <111489413+moshiloko@users.noreply.github.com>
2023-02-16 15:31:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          change by git
+          moshe got app
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
