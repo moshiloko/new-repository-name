209496f488b2de6a198a30e2a1d05cbc3c696a54
moshiloko <moshesamimi1999@gmail.com>
2023-02-19 11:41:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mosheeee the devops
+          mosheeee the devops perry
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
