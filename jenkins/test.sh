7761eca6fef6ea5c8a29ac7c103598c5183638d0
rafaf077 <111489413+moshiloko@users.noreply.github.com>
2023-02-21 14:42:27 +0200

    asdf

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -2,4 +2,4 @@ cd ..
 sudo npm run build
 # //move to jenkins username (sudo su jenkins)
 #pm2 serve build 3000 --spa  // server 1,2,3,4
-pm2 restart 0
+pm2 reload 0
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          is it working???????
+        working?
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
