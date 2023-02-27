2d1cfa0dfd986b789ec900e21c28f47cb71c0051
moshiloko <moshesamimi1999@gmail.com>
2023-02-27 11:45:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        is it balncing?
+        is it
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
