cab85a30d738f19df4fc5f4ba387e1d8a162b8b8
moshiloko <moshesamimi1999@gmail.com>
2023-02-28 10:19:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        is it
+        is it is it?
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
