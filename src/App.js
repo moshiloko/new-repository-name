578f27c2ccffe37fb5a97967eabca4759efaf83d
rafaf077 <111489413+moshiloko@users.noreply.github.com>
2023-03-05 09:57:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        dlfksaa;lkjdadw
+        hello
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
