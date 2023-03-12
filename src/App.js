148f3f1f9d3ea15e2197da59d539cd4eeda7d7e4
moshiloko <moshesamimi1999@gmail.com>
2023-03-12 11:22:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        zero down time
+        moshe
         </p>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
 
